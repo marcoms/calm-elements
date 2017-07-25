6b07e99e48fe87ee8fa10a431079b1073d19bedd
Marco Scannadinari <m@scannadinari.co.uk>
2017-07-25 21:50:00 +0100

    Add calm-base shared code file

---
+++ src/calm-base.ts
@@ -0,0 +1,76 @@
+export const fns = {
+	getStyleProp(el: HTMLElement, propName: string) {
+		return el.style.getPropertyValue(`--${propName}`);
+	},
+};
+
+export function makeFocusMixin(selector: string) {
+	return `
+		${selector} {
+			outline: none;
+		}
+
+		${selector}::after {
+			content: '';
+
+			position: absolute;
+			top: -4px;
+			left: -4px;
+
+			display: block;
+			width: calc(100% + 8px);
+			height: calc(100% + 8px);
+
+			box-shadow: 0 0 2px 2px var(--calm-color-focus);
+			border-radius: 8px;
+			opacity: 0;
+			transform: scale(1.1);
+
+			transition-property: transform, opacity;
+			transition-duration: 80ms;
+			transition-timing-function: linear;
+
+			pointer-events: none;
+		}
+
+		${selector}:focus::after {
+			transform: none;
+			opacity: 1;
+
+			transition-duration: 320ms;
+			transition-timing-function: var(--calm-ease-out);
+		}
+	`;
+}
+
+if (!(window as any).calmStylesInjected) {
+	const styleEl = document.createElement('style');
+	styleEl.innerHTML = `
+		:root {
+			--calm-color-blue: #2196f3;
+			--calm-color-blue-dark: #1976d2;
+
+			--calm-color-focus: rgba(25, 118, 210, 0.5);
+
+			--calm-color-active: rgba(0, 0, 0, 0.1);
+			--calm-color-active-darkbg: rgba(255, 255, 255, 0.1);
+
+			--calm-color-outline: rgba(0, 0, 0, 0.25);
+			--calm-color-outline-darkbg: rgba(255, 255, 255, 0.1);
+
+			--calm-ease-out: cubic-bezier(0.165, 0.84, 0.44, 1);
+			--calm-ease-in: cubic-bezier(0.895, 0.03, 0.685, 0.22);
+
+			--calm-duration-short: 160ms;
+			--calm-duration-medium: 320ms;
+			--calm-duration-long: 420ms;
+			--calm-duration-extreme: 960ms;
+
+			--calm-border-radius: 3px;
+		}
+	`;
+
+	document.head.appendChild(styleEl);
+
+	(window as any).calmStylesInjected = true;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,2 @@
-export const fns = {
-	getStyleProp(el: HTMLElement, propName: string) {
-		return el.style.getPropertyValue(`--${propName}`);
-	},
-};
-
-if (!(window as any).calmStylesInjected) {
-	const styleEl = document.createElement('style');
-	styleEl.innerHTML = `
-		:root {
-			--calm-ease-out: cubic-bezier(0.165, 0.84, 0.44, 1);
-			--calm-ease-in: cubic-bezier(0.895, 0.03, 0.685, 0.22);
-
-			--calm-duration-short: 160ms;
-			--calm-duration-medium: 320ms;
-			--calm-duration-long: 420ms;
-			--calm-duration-extreme: 960ms;
-
-			--calm-border-radius: 3px;
-		}
-	`;
-
-	(window as any).calmStylesInjected = true;
-}
+export { default as CalmTappable } from 'els/calm-tappable';
+export { default as CalmButton } from 'els/calm-button';
